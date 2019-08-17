a62dc8e029b1964053b1352347ce69192f2c11b9
Antonis Polykratis <polykratis_a@yahoo.gr>
2019-08-17 15:44:36 +0300

    log

--- model.py
+++ model.py
@@ -45,6 +45,8 @@ class EncoderModel(nn.Module):
                     print ("pretrained_vectors: ", pretrained_vectors.vectors[pt_idx].shape)
                     print ("type: ", type(pretrained_vectors.vectors[pt_idx]))
                     self.embed.weight[x].data.copy_(torch.from_numpy(pretrained_vectors.vectors[pt_idx]))
+                    print ("OK")
+        print ("== end init for EncoderModel")
 
     def init_weights(self):
         init_range = 0.1
